4ef1919ced5bb61472b78d8e51b6aa6ba6baaf95
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2024-06-28 14:17:12 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 96 ms, Memory: 13308 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n,m,r;
+vector<vector<int>> arr;
+int order[100001] ={};
+int visit[100001] ={};
+int cnt=1;
+
+void travel(int r){
+    visit[r] = 1;
+    order[r] = cnt++;
+    for(auto a : arr[r]){
+        if(!visit[a]){
+            travel(a);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n >> m >> r;
+    
+    int a,b;
+    int m2=m;
+    while(m--){
+        vector<int> vec;
+        arr.push_back(vec);
+    }
+    vector<int> vec;
+    arr.push_back(vec);
+    
+    while(m2--){
+        cin >> a >> b;
+        
+        arr[a].push_back(b);
+        arr[b].push_back(a);
+    }
+    
+    for(int i =0;i<=n;i++){
+        sort(arr[i].begin(), arr[i].end(), greater<>());
+    }
+    
+    travel(r);
+    
+    for(int i=1; i<=n;i++){
+        cout << order[i] << '\n';
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
