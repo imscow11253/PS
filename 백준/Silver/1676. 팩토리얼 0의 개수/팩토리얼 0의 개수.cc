5454bf257685685adbd02fcd6a1bc588a9927067
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2024-09-04 21:31:35 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int result = 0;
+    for (int i = 5; i <= n; i *= 5) {
+        result += n / i;
+    }
+    
+    cout << result << endl;
+    return 0;
+}
