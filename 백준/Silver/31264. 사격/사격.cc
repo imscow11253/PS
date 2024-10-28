22abf7109912a55453dcd46376ef7cffe22e57cb
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2024-10-28 13:24:59 +0900

    [Silver I] Title: 사격, Time: 28 ms, Memory: 2416 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31264. 사격/사격.cc
+++ 백준/Silver/31264. 사격/사격.cc
@@ -4,44 +4,50 @@
 
 using namespace std;
 
-int n,m,a;
+int n, m;
+long long a;
 vector<int> vec;
 
-int main(){
+bool canPromote(int initial_ability) {
+    int current_ability = initial_ability;
+    long long total_score = 0;
+
+    for (int i = 0; i < m; ++i) {
+        auto index = upper_bound(vec.begin(), vec.end(), current_ability);
+        if (index == vec.begin()) break;
+        --index;
+        int target_score = *index;
+        
+        total_score += target_score;
+        current_ability += target_score;
+        
+        if (total_score >= a) return true;
+    }
+    return total_score >= a;
+}
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     cin >> n >> m >> a;
-    int temp;
-    for(int i =0;i<n;i++){
-        cin >> temp;
-        vec.push_back(temp);
+    vec.resize(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> vec[i];
     }
     sort(vec.begin(), vec.end());
-        
-    int ability=1;
-    while(true){
-        int temp_ability=ability;
-        int temp_score=0;
-        for(int i=0;i<m;i++){
-            auto index = upper_bound(vec.begin(),vec.end(),temp_ability);
-            if(index != vec.begin()){
-                temp_ability += *(index-1);
-                temp_score += *(index-1);
-            }
-            else{
-                break;
-            }
-        }
-        if(temp_score >= a){
-            cout << ability;
-            break;
-        }
-        else{
-            ability++;
-            continue;
+    
+    int low = 0, high = vec[n - 1], answer = high;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (canPromote(mid)) {
+            answer = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
         }
     }
     
+    cout << answer << '\n';
     return 0;
 }
