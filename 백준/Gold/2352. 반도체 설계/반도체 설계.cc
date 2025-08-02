0c3ff2c53d79bf9e54a25f4eab979d41bfcbc3fe
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2025-08-03 01:48:59 +0900

    [Gold II] Title: 반도체 설계, Time: 4 ms, Memory: 2532 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vector<int> lis;
+    
+    int temp;
+    for(int i=0;i<n;i++){
+        cin >> temp;
+        auto it = lower_bound(lis.begin(), lis.end(), temp);
+        if(it == lis.end()) lis.push_back(temp);
+        else *it = temp;
+    }
+    cout << lis.size();
+    
+    return 0;
+}
