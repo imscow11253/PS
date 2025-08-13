7985a233ad90e398de2a5cc0c1cadd82c9c38128
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2025-08-14 01:18:45 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    long long arr[91];
+    arr[0] =0; arr[1]=1;
+    for(int i=2;i<=n;i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+    cout << arr[n];
+    
+    return 0;
+}
