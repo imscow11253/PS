25b771d5029ef26914bd740aad72da1564cb52a4
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2026-01-04 14:37:31 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 9348 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.js
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.js
@@ -1,23 +1,24 @@
-const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split("\n");
+const fs=require("fs");
+const input=fs.readFileSync(0).toString().trim().split('\n');
 
-const n = Number(input.shift());
-const list = input.map(Number);
+const n=Number(input.shift());
+const list=input.map(Number);
 
-const solution = (n) => {
-  if (n === 1) return list[0];
-  if (n === 2) return list[0] + list[1];
+const dp=Array(n).fill(0);
+if(n==1) {
+  console.log(list[0]);
+  return;
+}
+if(n==2){
+  console.log(list[0]+list[1]);
+  return;
+}
+dp[0]=list[0];
+dp[1]=list[0]+list[1];
+dp[2]=Math.max(dp[0], list[1])+list[2];
 
-  let dp = new Array(n).fill(0);
-  dp[0] = list[0];
-  dp[1] = list[0] + list[1];
-  dp[2] = Math.max(list[0] + list[2], list[1] + list[2]);
+for(let i=3; i<n; i++){
+  dp[i]=Math.max(dp[i-3]+list[i-1], dp[i-2])+list[i];
+}
 
-  for (let i = 3; i < n; i++) {
-    dp[i] = Math.max(dp[i - 3] + list[i - 1] + list[i], dp[i - 2] + list[i]);
-  }
-
-  return dp[n - 1];
-};
-
-console.log(solution(n));
+console.log(dp[n-1]);
