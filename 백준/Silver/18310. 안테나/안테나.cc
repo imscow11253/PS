5b90a2798b390e2e3a41bfab761769f574bcad9d
Kwon MinHyeok <71692903+imscow11253@users.noreply.github.com>
2026-03-08 22:07:05 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
+  int n;
+  cin >> n;
+  vector<int> vec(n);
+  for(int i =0;i<n;i++){
+    cin >> vec[i];
+  }
+  sort(vec.begin(), vec.end());
+  
+  cout << vec[(n-1)/2];
+  
+  return 0;
+}
